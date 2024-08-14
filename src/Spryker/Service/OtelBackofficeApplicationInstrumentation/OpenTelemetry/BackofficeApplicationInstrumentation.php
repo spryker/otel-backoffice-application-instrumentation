54ed575e5e03354fde0811265c2ce0b4b4c8f3a5
Olha Livitchuk <olha.livitchuk@spryker.com>
2024-08-14 16:56:13 +0200

    FRW-8618 Adjustments

--- src/Spryker/Service/OtelBackofficeApplicationInstrumentation/OpenTelemetry/BackofficeApplicationInstrumentation.php
+++ src/Spryker/Service/OtelBackofficeApplicationInstrumentation/OpenTelemetry/BackofficeApplicationInstrumentation.php
@@ -23,7 +23,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Throwable;
 use function OpenTelemetry\Instrumentation\hook;
 
-class BackofficeApplicationInstrumentation implements BackofficeApplicationInstrumentationInterface
+class BackofficeApplicationInstrumentation
 {
     /**
      * @var string
